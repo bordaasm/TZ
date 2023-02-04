20ec4e21aec6290f7c1176da55d6ecbd2929dac3
bordaasm <124432382+bordaasm@users.noreply.github.com>
2023-02-04 17:53:59 +0800

    tz

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1">
+    <title>Tzeezotje</title>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/form.js"></script>
+    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="media.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+<input type="checkbox" id="nav-toggle" hidden>
+
+<nav class="nav">
+
+    <label for="nav-toggle" class="nav-toggle" onclick></label>
+
+    <div class="Pmenu" >
+        <ul class="um">
+
+
+
+            <li><a href="#">À la carte</a></li>
+            <div class="line"></div>
+            <li><a href="#">Wijnkaart</a></li>
+            <div class="line"></div>
+            <li><a href="#">Lunchmenu</a></li>
+            <div class="line"></div>
+            <li><a href="#">Contact</a></li>
+            <div class="line"></div>
+            <div class="num">
+                <img src="img/Group%204.svg" alt="">
+                <h4>+32 9 282 57 25</h4>
+            </div>
+            <div class="snum">
+                <div class="FB"><a href="#"><img src="img/FB.svg"></a></div>
+                <div class="IG"><a href="#"><img src="img/IG.svg"></a></div>
+                <div class="TW"><a href="#"><img src="img/TW.svg"></a></div>
+                <div class="YT"><a href="#"><img src="img/YT.svg"></a></div>
+            </div>
+        </ul>
+    </div>
+    </ul>
+</nav>
+<!--
+<div class="Pmenu" >
+    <ul class="um">
+
+        <div class="cancel">
+            <a href="#"><img src="img/x-solid%201.svg" alt=""></a>
+        </div>
+
+    <li><a href="#">À la carte</a></li>
+        <div class="line"></div>
+    <li><a href="#">Wijnkaart</a></li>
+        <div class="line"></div>
+    <li><a href="#">Lunchmenu</a></li>
+        <div class="line"></div>
+    <li><a href="#">Contact</a></li>
+        <div class="line"></div>
+        <div class="num">
+            <img src="img/Group%204.svg" alt="">
+            <h4>+32 9 282 57 25</h4>
+        </div>
+        <div class="snum">
+        <div class="FB"><a href="#"><img src="img/FB.svg"></a></div>
+        <div class="IG"><a href="#"><img src="img/IG.svg"></a></div>
+        <div class="TW"><a href="#"><img src="img/TW.svg"></a></div>
+        <div class="YT"><a href="#"><img src="img/YT.svg"></a></div>
+        </div>
+    </ul>
+</div>
+-->
+
+
+    <div class="main">
+        <div class="header">
+            <div class="logo"><img src="img/NAAM%201.svg" alt=""></div>
+            <div class="navigation">
+                <a href="#">À la carte</a>
+                <a href="#">Wijnkaart</a>
+                <a href="#">Lunchmenu</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="number"><img src="img/Group%204.svg" alt="">
+                <h4>+32 9 282 57 25</h4>
+            </div>
+        </div>
+        <div class="info">
+            <h3>OPEN EVERYDAY 6AM - 9PM</h3>
+            <h1>A bad day with lunch is better than a good day without it.</h1>
+        </div>
+        <div class="order">
+            <div class="btn" >
+            <a href="#">
+                <div class="order_b" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">
+                    <h4>Reserve a table</h4>
+                </div>
+            </a>
+            </div>
+            <div class="sn">
+               <div class="FB"><a href="#"><img src="img/FB.svg"></a></div>
+               <div class="IG"><a href="#"><img src="img/IG.svg"></a></div>
+               <div class="TW"><a href="#"><img src="img/TW.svg"></a></div>
+               <div class="YT"><a href="#"><img src="img/YT.svg"></a></div>
+            </div>
+        </div>
+    </div>
+    <div class="location">
+        <div class="restaurants">
+            <div class="address">
+
+
+                <h3>Woensdag 12/01</h3>
+                <div class="text_address">
+                    <h5>Salade met Serrano</h5>
+                    <img src="img/Group%203389.svg">
+                    <h5>Steak Tartaar</h5>
+                </div>
+                <div class="line"></div>
+
+
+                <h3>Donderdag 13/01</h3>
+                <div class="text_address">
+                    <h5>Toast Roquefort</h5>
+                    <img src="img/Group%203389.svg">
+                    <h5>Penne met Kip en Prei</h5>
+                </div>
+                <div class="line"></div>
+
+
+                <h3>Vrijdag 14/01</h3>
+                <div class="text_address">
+                    <h5>Verrassing van de Chef</h5>
+                    <img src="img/Group%203389.svg">
+                    <h5>Penne met Kip en Prei</h5>
+                </div>
+                <div class="line"></div>
+
+
+                <h3>Vrijdag 15/01</h3>
+                <div class="text_address">
+                    <h5>Verrassing van de Chef</h5>
+                    <img src="img/Group%203389.svg">
+                    <h5>Penne met Kip en Prei</h5>
+                </div>
+                <div class="line"></div>
+
+
+                <h3>Vrijdag 16/01</h3>
+                <div class="text_address">
+                    <h5>Verrassing van de Chef</h5>
+                    <img src="img/Group%203389.svg">
+                    <h5>Penne met Kip en Prei</h5>
+                </div>
+                <div class="line"></div>
+
+
+            </div>
+            <div class="pictures">
+                <img src="img/Group%203390.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="slider_page">
+        <div class="slider">
+            <swiper-container navigation="true" >
+                <swiper-slide>
+                    <div class="slide">
+                        <img src="img/2.svg" alt="">
+                        <h3>Goede service, lekker eten, toffe ambiance</h3>
+                        <div class="text">
+                            <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  dolores et ea rebum. Stet clita kasd gubergren.</h5>
+                        </div>
+                        <h4>ALAIN HEYNDRICKX</h4>
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="slide">
+                        <img src="img/2.svg" alt="">
+                        <h3>Goede service, lekker eten, toffe ambiance</h3>
+                        <div class="text">
+                            <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  dolores et ea rebum. Stet clita kasd gubergren.</h5>
+                        </div>
+                        <h4>ALAIN HEYNDRICKX</h4>
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="slide">
+                        <img src="img/2.svg" alt="">
+                        <h3>Goede service, lekker eten, toffe ambiance</h3>
+                        <div class="text">
+                            <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  dolores et ea rebum. Stet clita kasd gubergren.</h5>
+                        </div>
+                        <h4>ALAIN HEYNDRICKX</h4>
+                    </div>
+                </swiper-slide>
+            </swiper-container>
+        </div>
+    </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Зарезервировать столик</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+            </div>
+            <div class="modal-body">
+                <div class="forms">
+                    <form id="form" action="form.php" method="post" >
+                        <input type="text" name="name" placeholder="Ваше имя" required>
+                        <input type="number" name="tel" placeholder="Ваш номер телефона" required>
+                        <input type="datetime-local" name="date_time" required>
+                        <input type="submit" name="send" value="Отправить">
+                    </form>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ js/form.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+    $(`form`).submit(function( event ) {
+        event.preventDefault();
+
+        $.ajax({
+            url: $(this).attr(`action`),
+            type: $(this).attr(`method`),
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (result){
+                alert(`Столик забронирован`);
+            }
+        });
+
+
+    });
+});
